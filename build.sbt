e8f1c288abd57e459720a068da985c0d65f4925c
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-04-19 22:59:49 +0100

    simple sbt file and few shell samples

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "proj organizer"
+
+version := "0.1"
+
+// scalaVersion := "2.9.2"
--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -5,6 +5,22 @@ object HelloScala {
     println("Hello, Scala!")
     println("ls -l".!)
     val contents = Process("ls -l").lines
-    println(contents)
+    contents foreach println
   }
 }
+
+
+// make it whitespace-safe
+// def contentsOf(dir: String): String = Seq("ls", dir).!!
+
+// more complex example
+// import scala.sys.process._
+// "find src -name *.scala -exec grep null {} ;"  #|  "xargs test -z"  #&&  "echo null-free"  #||  "echo null detected"  !
+
+// Note that every String is implicitly converted into a ProcessBuilder through the implicits imported from process. These ProcessBuilder are then combined in three different ways.
+
+//     #| pipes the output of the first command into the input of the second command. It mirrors a shell pipe (|).
+//     #&& conditionally executes the second command if the previous one finished with exit value 0. It mirrors shell's &&.
+//     #|| conditionally executes the third command if the exit value of the previous command is different than zero. It mirrors shell's &&.
+
+// Finally, ! at the end executes the commands, and returns the exit value. Whatever is printed will be sent to the Scala process standard output. If we wanted to caputre it, we could run that with !! instead.
